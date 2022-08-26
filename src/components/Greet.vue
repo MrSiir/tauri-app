792a82895aa410891a2bd28278bfdceb9b85522c
Xavier Ferrón <javi@iquadrat.com>
2022-08-26 14:20:52 +0200

    first commit

---
+++ src/components/Greet.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from "vue";
+import { invoke } from "@tauri-apps/api/tauri";
+
+const greetMsg = ref("");
+const name = ref("");
+
+async function greet() {
+  greetMsg.value = await invoke("greet", { name: name.value });
+}
+</script>
+
+<template>
+  <div class="card">
+    <input v-model="name" placeholder="Introduce tu nombre..." />
+    <button type="button" @click="greet()">Saludar</button>
+  </div>
+
+  <p>{{ greetMsg }}</p>
+</template>
